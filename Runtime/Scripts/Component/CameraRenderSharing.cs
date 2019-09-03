a657edb843644104e1c9a0788a180f585adf27fd
Stella Cannefax <stella@unity3d.com>
2019-09-02 22:04:10 -0700

    add camera render sharing script

---
+++ Runtime/Scripts/Component/CameraRenderSharing.cs
@@ -0,0 +1,42 @@
+﻿using Klak.Spout;
+using UnityEngine;
+
+namespace Resolink
+{
+    public class CameraRenderSharing : MonoBehaviour
+    {
+        [Tooltip("The camera to share a render texture to Resolume from. Main Camera if not specified")]
+        public Camera CameraToShare;
+
+        void OnEnable()
+        {
+            GetCameraRef();
+            EnsureSendingComponent();
+        }
+
+        void Start()
+        {
+            GetCameraRef();
+            EnsureSendingComponent();
+        }
+
+        void GetCameraRef()
+        {
+            if (CameraToShare == null)
+                CameraToShare = Camera.main;
+            if (CameraToShare == null)
+                CameraToShare = Camera.current;
+        }
+
+        public void EnsureSendingComponent()
+        {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+            var foundComponent = CameraToShare.GetComponent<SpoutSender>();
+            if (foundComponent == null)
+            {
+                CameraToShare.gameObject.AddComponent<SpoutSender>();
+            }
+#endif
+        }
+    }
+}
