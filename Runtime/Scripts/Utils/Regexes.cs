3aad12e4af518d835c84f1ade4e05b8d162d9101
Stella Cannefax <stella@unity3d.com>
2019-09-03 22:42:08 -0700

    start introducing proper regexes with tests

--- Runtime/Scripts/Utils/Regexes.cs
+++ Runtime/Scripts/Utils/Regexes.cs
@@ -2,11 +2,27 @@ using System.Text.RegularExpressions;
 
 namespace Resolink
 {
+    public class RegexStrings
+    {
+        public const string ConnectClip = "layers/[0-9]+/connect[a-z]+clip$";
+        
+        public const string BehaviorGain = "/[a-z]+/behaviour/gain$";
+        public const string BehaviorFallback = "/[a-z]+/behaviour/fallback$";
+        
+        
+        public const string BehaviorDuration = "/[a-z]+/behaviour/duration$";
+
+        public const string DashboardLink = "/dashboard/link[1-8]$";
+
+    }
+
     public static class Regexes
     {
-        const string k_NumberRegexPattern = "\\/([0-9]+)\\/";
+        const string k_NumberRegexPattern = "/([0-9]+)/";
         
-        const string k_LayerNumberRegexPattern = "layers\\/([0-9]+)\\/";
+        const string k_LayerNumberRegexPattern = "layers/([0-9]+)/";
+
+
 
         public static Regex WildcardNumber { get; } = new Regex(k_NumberRegexPattern);
         public static Regex LayerNumber { get; } = new Regex(k_LayerNumberRegexPattern);
---
+++ Tests/Editor/RegexTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace Resolink.Tests
+{
+    public class RegexTests
+    {
+        [TestCase("/composition/layers/4/connectprevclip")]
+        [TestCase("/composition/layers/3/connectnextclip")]
+        [TestCase("/composition/layers/2/connectspecificclip")]
+        public void ConnectClip(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.ConnectClip, input, match);
+        }
+        
+        [TestCase("/composition/layers/1/video/effects/transform/scalew/behaviour/gain")]
+        [TestCase("/composition/layers/2/video/effects/transform/positiony/behaviour/gain")]
+        [TestCase("/composition/layers/3/video/effects/delayrgb/opacity/behaviour/gain")]
+        [TestCase("/composition/layers/4/video/opacity/behaviour/gain")]
+        [TestCase("/composition/layers/4/video/opacity/behaviour/gain/something/after", false)]
+        public void BehaviorGain(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.BehaviorGain, input, match);
+        }
+        
+        [TestCase("/composition/layers/4/video/effects/transform/anchorx/behaviour/fallback")]
+        [TestCase("/composition/layers/3/video/effects/transform/positiony/behaviour/fallback")]
+        [TestCase("/composition/layers/2/video/effects/transform/scaleh/behaviour/fallback")]
+        [TestCase("/composition/layers/1/video/opacity/behaviour/fallback")]
+        [TestCase("/composition/layers/1/video/opacity/behaviour/fallback/something/after", false)]
+        public void BehaviorFallback(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.BehaviorFallback, input, match);
+        }
+        
+        
+        [TestCase("/composition/dashboard/link1")]
+        [TestCase("/composition/dashboard/link8")]
+        [TestCase("/layers/1/dashboard/link4")]
+        [TestCase("/layers/1/dashboard/link", false)]
+        [TestCase("/composition/dashboard/link9", false)]
+        public void DashboardLink(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.DashboardLink, input, match);
+        }
+
+
+
+        void AssertRegexMatch(Regex regex, string input, bool match = true)
+        {
+            if(match)
+                Assert.True(regex.IsMatch(input), $"Expected {regex} to match {input}, but it did not");
+            else
+                Assert.False(regex.IsMatch(input), $"Expected {regex} to not match {input}, but it did");
+        }
+        
+        void AssertRegexMatch(string pattern, string input, bool match = true)
+        {
+            var regex = new Regex(pattern);
+            if(match)
+                Assert.True(regex.IsMatch(input), $"Expected {regex} to match {input}, but it did not");
+            else
+                Assert.False(regex.IsMatch(input), $"Expected {regex} to not match {input}, but it did");
+        }
+    }
+}
