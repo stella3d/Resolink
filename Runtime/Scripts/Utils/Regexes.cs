2b851b4e79e672b91d7fd4484d81f607da66dde5
Stella Cannefax <stella@unity3d.com>
2019-09-04 00:05:35 -0700

    add a lot more regexes and tests for them

--- Runtime/Scripts/Utils/Regexes.cs
+++ Runtime/Scripts/Utils/Regexes.cs
@@ -4,16 +4,27 @@ namespace Resolink
 {
     public class RegexStrings
     {
+        public const string ApplicationClipScroll = "/application/ui/clipsscroll(horizontal|vertical)$";
+
         public const string ConnectClip = "layers/[0-9]+/connect[a-z]+clip$";
+        public const string ConnectSemanticColumn = "^/composition/connect(prev|next|specific)column$";
+        
+        public const string CuepointsSet = "/cuepoints/setparams/set[1-6]$";
+        public const string CuepointsJump = "/cuepoints/jumpparams/jump[1-6]$";
         
         public const string BehaviorGain = "/[a-z]+/behaviour/gain$";
         public const string BehaviorFallback = "/[a-z]+/behaviour/fallback$";
+
+        public const string BypassedSoloClear = "/*/(bypassed|solo|clear)$";
         
         
-        public const string BehaviorDuration = "/[a-z]+/behaviour/duration$";
+        public const string BehaviorDuration = "/[a-z]+/behaviour/duration(/(divide|multiply))?$";
 
         public const string DashboardLink = "/dashboard/link[1-8]$";
 
+        public const string TempoTapPullPush = "/tempocontroller/tempo(tap|pull|push)$";
+
+        public const string TransportPosition = "/transport/position(/(in|out))?$";
     }
 
     public static class Regexes
--- Tests/Editor/RegexTests.cs
+++ Tests/Editor/RegexTests.cs
@@ -14,6 +14,42 @@ namespace Resolink.Tests
             AssertRegexMatch(RegexStrings.ConnectClip, input, match);
         }
         
+        [TestCase("/composition/connectprevcolumn")]
+        [TestCase("/composition/connectnextcolumn")]
+        [TestCase("/composition/connectspecificcolumn")]
+        public void ConnectSemanticColumn(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.ConnectSemanticColumn, input, match);
+        }
+        
+        
+        
+        [TestCase("/composition/layers/6/clips/1/transport/cuepoints/setparams/set1")]
+        [TestCase("/composition/layers/5/clips/2/transport/cuepoints/setparams/set2")]
+        [TestCase("/composition/layers/4/clips/3/transport/cuepoints/setparams/set3")]
+        [TestCase("/composition/layers/3/clips/4/transport/cuepoints/setparams/set4")]
+        [TestCase("/composition/layers/2/clips/5/transport/cuepoints/setparams/set5")]
+        [TestCase("/composition/layers/1/clips/6/transport/cuepoints/setparams/set6")]
+        [TestCase("/composition/layers/1/clips/6/transport/cuepoints/setparams/set", false)]
+        [TestCase("/composition/layers/1/clips/6/transport/cuepoints/setparams/set7", false)]
+        public void CuepointsSet(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.CuepointsSet, input, match);
+        }
+        
+        [TestCase("/composition/layers/6/clips/1/transport/cuepoints/jumpparams/jump1")]
+        [TestCase("/composition/layers/5/clips/2/transport/cuepoints/jumpparams/jump2")]
+        [TestCase("/composition/layers/4/clips/3/transport/cuepoints/jumpparams/jump3")]
+        [TestCase("/composition/layers/3/clips/4/transport/cuepoints/jumpparams/jump4")]
+        [TestCase("/composition/layers/2/clips/5/transport/cuepoints/jumpparams/jump5")]
+        [TestCase("/composition/layers/1/clips/6/transport/cuepoints/jumpparams/jump6")]
+        [TestCase("/composition/layers/1/clips/6/transport/cuepoints/jumpparams/jump", false)]
+        [TestCase("/composition/layers/1/clips/6/transport/cuepoints/jumpparams/jump7", false)]
+        public void CuepointJump(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.CuepointsJump, input, match);
+        }
+        
         [TestCase("/composition/layers/1/video/effects/transform/scalew/behaviour/gain")]
         [TestCase("/composition/layers/2/video/effects/transform/positiony/behaviour/gain")]
         [TestCase("/composition/layers/3/video/effects/delayrgb/opacity/behaviour/gain")]
@@ -34,6 +70,17 @@ namespace Resolink.Tests
             AssertRegexMatch(RegexStrings.BehaviorFallback, input, match);
         }
         
+        // we group these together because they fall in the same category and have the same type + very similar paths
+        [TestCase("/composition/layers/1/bypassed")]
+        [TestCase("/composition/layers/2/solo")]
+        [TestCase("/composition/layers/3/clear")]
+        [TestCase("/composition/bypassed")]
+        [TestCase("/composition/someeffectprobably/bypassed")]
+        [TestCase("/compositionbypassed", false)]
+        public void BypassedSoloClear(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.BypassedSoloClear, input, match);
+        }
         
         [TestCase("/composition/dashboard/link1")]
         [TestCase("/composition/dashboard/link8")]
@@ -44,8 +91,32 @@ namespace Resolink.Tests
         {
             AssertRegexMatch(RegexStrings.DashboardLink, input, match);
         }
+        
+        
+        [TestCase("/application/ui/clipsscrollhorizontal")]
+        [TestCase("/application/ui/clipsscrollvertical")]
+        public void ClipScroll(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.ApplicationClipScroll, input, match);
+        }
 
-
+        [TestCase("/composition/tempocontroller/tempotap")]
+        [TestCase("/composition/tempocontroller/tempopull")]
+        [TestCase("/composition/tempocontroller/tempopush")]
+        public void TempoTapPullPush(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.TempoTapPullPush, input, match);
+        }
+        
+        [TestCase("/composition/layers/*/clips/*/transport/position")]
+        [TestCase("/composition/layers/*/clips/*/transport/position/in")]
+        [TestCase("/composition/layers/*/clips/*/transport/position/out")]
+        [TestCase("/composition/layers/*/clips/*/transport/position/out/something", false)]
+        [TestCase("/composition/layers/*/clips/*/transport/position/no", false)]
+        public void TransportPosition(string input, bool match = true)
+        {
+            AssertRegexMatch(RegexStrings.TransportPosition, input, match);
+        }
 
         void AssertRegexMatch(Regex regex, string input, bool match = true)
         {
