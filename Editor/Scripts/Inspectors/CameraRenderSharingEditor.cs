a2e99c49c26d62430d8e9455dc6e7380e0b1d486
Stella Cannefax <stella@unity3d.com>
2019-09-04 21:26:31 -0700

    custom editor for camera render sharing

---
+++ Editor/Scripts/Inspectors/CameraRenderSharingEditor.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEditor;
+
+namespace Resolink
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(CameraRenderSharing))]
+    public class CameraRenderSharingEditor : Editor
+    {
+        const string k_HelpText = "This component ensures that the right component to share video out of Unity " +
+                                  "has been attached to the Camera object.";
+
+        SerializedProperty m_CameraProperty;
+        
+        public void OnEnable()
+        {
+            m_CameraProperty = serializedObject.FindProperty("CameraToShare");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.HelpBox(k_HelpText, MessageType.Info);
+            EditorGUILayout.PropertyField(m_CameraProperty);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
