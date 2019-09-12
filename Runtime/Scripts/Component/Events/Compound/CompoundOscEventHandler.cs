f41e2d401a5c8a2548cb9205a54b8f07d004da53
Stella Cannefax <stella@unity3d.com>
2019-09-12 03:41:16 -0700

    summary comments for CompoundOscEventHandler

--- Runtime/Scripts/Component/Events/Compound/CompoundOscEventHandler.cs
+++ Runtime/Scripts/Component/Events/Compound/CompoundOscEventHandler.cs
@@ -2,25 +2,34 @@ using System;
 using OscJack;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Serialization;
 
 namespace Resolink
 {
-    public class CompoundOscEventHandler : MonoBehaviour { }
+    public abstract class CompoundOscEventHandler : MonoBehaviour { }
     
+    /// <summary>
+    /// Handles grouping together primitive events into an event of a more complex data type
+    /// </summary>
+    /// <typeparam name="THandler">The type of Action-based address handler for each sub-event</typeparam>
+    /// <typeparam name="TEvent">The type of UnityEvent-based handler for the single complex event</typeparam>
+    /// <typeparam name="TCompoundData">The complex type of data TEvent takes</typeparam>
+    /// <typeparam name="TComponentData">The primitive type of data sub-events take</typeparam>
     [HelpURL(HelpLinks.TableOfContents)]
-    public abstract class CompoundOscEventHandler<THandler, TEvent, TCompoundData, TComponentData> : CompoundOscEventHandler
+    public abstract class CompoundOscEventHandler<THandler, TEvent, TCompoundData, TComponentData> : 
+        CompoundOscEventHandler
         where TEvent: UnityEvent<TCompoundData>, new()
         where THandler: OscActionHandler<TComponentData>
     {
+        /// <summary>The value that will be passed to the UnityEvent</summary>
         public TCompoundData Value;
         
+        /// <summary>The UnityEvent that takes the complex data type</summary>
         public TEvent Event;
         
+        /// <summary>All handlers for sub-events, associated with a Resolume shortcut</summary>
         public THandler[] Handlers;
         
         protected bool m_Registered;
-
         protected bool m_Dirty;
         
         public void OnEnable()
@@ -40,7 +49,9 @@ namespace Resolink
                 Register();
         }
         
-        public void Update()
+        // We use LateUpdate instead of Update here so that we can be sure it runs after the OscRouter.
+        // The OscRouter runs all of the callbacks that might set the dirty flag in Update.
+        public void LateUpdate()
         {
             if (m_Dirty)
             {            
@@ -87,8 +98,8 @@ namespace Resolink
         /// <summary>
         /// When one of the component handlers fires, we also want to set the dirty state so the combined event fires.
         /// </summary>
-        /// <param name="handler"></param>
-        /// <returns></returns>
+        /// <param name="handler">The event handler to wrap</param>
+        /// <returns>A method that calls the wrapped function and sets the dirty flag</returns>
         Action<OscDataHandle> GetWrapperForComponentEvent(OscActionHandler<TComponentData> handler)
         {
             return handle =>
