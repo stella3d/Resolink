fa5d30938a1d4010a89a4dd92dd7594a945f931c
Stella Cannefax <stella@unity3d.com>
2019-09-11 19:00:24 -0700

    add platform-specific component names to camera sharing script help

--- Editor/Scripts/Inspectors/CameraRenderSharingEditor.cs
+++ Editor/Scripts/Inspectors/CameraRenderSharingEditor.cs
@@ -6,8 +6,16 @@ namespace Resolink
     [CustomEditor(typeof(CameraRenderSharing))]
     public class CameraRenderSharingEditor : Editor
     {
-        const string k_HelpText = "This component ensures that the right component to share video out of Unity " +
-                                  "has been attached to the Camera object.";
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        const string CameraComponentName = "SpoutSender";
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+        const string CameraComponentName = "SyphonServer";
+#else
+        const string CameraComponentName = "UNSUPPORTED PLATFORM";
+#endif
+        
+        const string k_HelpText = "This component ensures that a " + CameraComponentName + " component has been " +
+                                  "attached to the scene's active Camera object, to share video out of Unity.";
 
         SerializedProperty m_CameraProperty;
         
@@ -15,7 +23,7 @@ namespace Resolink
         {
             m_CameraProperty = serializedObject.FindProperty("CameraToShare");
         }
-
+        
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
