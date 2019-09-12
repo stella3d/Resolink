e146dcd6322aeb22a3055263f47151daf183cb9e
Stella Cannefax <stella@unity3d.com>
2019-09-12 03:11:42 -0700

    add settings provider support for grouping settings

--- Editor/Scripts/ResolinkEditorSettings.cs
+++ Editor/Scripts/ResolinkEditorSettings.cs
@@ -20,18 +20,21 @@ namespace Resolink
             private set => s_Instance = value;
         }
         
-        public const string k_ResolinkSettingsFallbackPath = "Assets/ResolinkEditorSettings.asset";
-
 #pragma warning disable 649
-        [SerializeField]
-        bool m_ShowHelp;
+        [SerializeField] bool m_ShowHelp;
+        [SerializeField] bool m_WarnOnUnknownType;
         
-        [SerializeField] 
-        bool m_WarnOnUnknownType;
+        [SerializeField] bool m_GroupColors;
+        [SerializeField] bool m_GroupVector2s;
+        [SerializeField] bool m_GroupVector3s;
 #pragma warning restore 649
 
         public bool ShowHelp => m_ShowHelp;
         public bool WarnOnUnknownType => m_WarnOnUnknownType;
+        
+        public bool GroupColors => m_GroupColors;
+        public bool GroupVector2s => m_GroupVector2s;
+        public bool GroupVector3s => m_GroupVector3s;
 
         public void OnEnable()
         {
@@ -43,8 +46,9 @@ namespace Resolink
             var settings = GetSettingsFromAssets();
             if (settings == null)
             {
+                const string settingsFallbackPath = "Assets/ResolinkEditorSettings.asset";
                 settings = CreateInstance<ResolinkEditorSettings>();
-                AssetDatabase.CreateAsset(settings, k_ResolinkSettingsFallbackPath);
+                AssetDatabase.CreateAsset(settings, settingsFallbackPath);
                 AssetDatabase.SaveAssets();
             }
             return settings;
@@ -68,13 +72,29 @@ namespace Resolink
     {
         static readonly GUIContent ShowHelpContent = new GUIContent("Show Help", 
             "If enabled, help boxes are shown on most Resolink components");
-        
+
         static readonly GUIContent TypeWarningContent = new GUIContent("Warn on Unknown Type", 
-            "If enabled, warnings will be shown in the console for Resolume shortcuts we don't know the data type for");
+            "If enabled, warnings will be shown in the console for Resolume shortcuts we don't know the data type " +
+            "for when we parse the OSC map.");
+
+        const string k_FloatSuffix = " event handler, as opposed to being individual float handlers.";
         
+        static readonly GUIContent GroupColorsContent = new GUIContent("Group Colors", 
+            "If enabled, r/g/b/a messages belonging to the same Color in Resolume get grouped into a Color" 
+            + k_FloatSuffix);
+        static readonly GUIContent GroupVector2sContent = new GUIContent("Group Vector2s", 
+            "If enabled, x/y messages belonging to the same 2d vector in Resolume get grouped into a Vector2 " 
+            + k_FloatSuffix);
+        static readonly GUIContent GroupVector3sContent = new GUIContent("Group Vector3s", 
+            "If enabled, x/y/z messages belonging to the same 3d vector in Resolume get grouped into a Vector3" 
+            + k_FloatSuffix);
+
+        static bool s_GroupingFoldoutState = true;
+
         [SettingsProvider]
         public static SettingsProvider CreateResolinkSettingsProvider()
         {
+            
             // First parameter is the path in the Settings window.
             var provider = new SettingsProvider("Project/Resolink", SettingsScope.User)
             {
@@ -87,11 +107,26 @@ namespace Resolink
                     settings.Update();
                     EditorGUILayout.PropertyField(settings.FindProperty("m_ShowHelp"), ShowHelpContent);
                     EditorGUILayout.PropertyField(settings.FindProperty("m_WarnOnUnknownType"), TypeWarningContent);
+
+                    s_GroupingFoldoutState = EditorGUILayout.Foldout(s_GroupingFoldoutState, "Complex Data Types");
+                    if (s_GroupingFoldoutState)
+                    {
+                        using (new EditorGUI.IndentLevelScope())
+                        {
+                            var colorProp = settings.FindProperty("m_GroupColors");
+                            EditorGUILayout.PropertyField(colorProp, GroupColorsContent);
+                            var vec2Prop = settings.FindProperty("m_GroupVector2s");
+                            EditorGUILayout.PropertyField(vec2Prop, GroupVector2sContent);
+                            var vec3Prop = settings.FindProperty("m_GroupVector3s");
+                            EditorGUILayout.PropertyField(vec3Prop, GroupVector3sContent);
+                        }
+                    }
+
                     settings.ApplyModifiedProperties();
                 },
 
                 // Populate the search keywords to enable smart search filtering and label highlighting:
-                keywords = new HashSet<string>(new[] { "Resolink" })
+                keywords = new HashSet<string>(new[] { "Resolink", "Resolume" })
             };
 
             return provider;
