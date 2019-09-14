6ccdb0e6e645b99f694a28fb73b9505cc296c993
Stella Cannefax <stella@unity3d.com>
2019-09-14 13:28:33 -0700

    add test maps folder

---
+++ Tests/Editor/Maps/ExpectedParseResult.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Resolink.Tests
+{
+    [CreateAssetMenu]
+    public class ExpectedParseResult : ScriptableObject
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
