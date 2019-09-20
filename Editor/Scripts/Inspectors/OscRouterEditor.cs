1cfc0d57fac76adae405d621aa02a6ff8842d922
Stella Cannefax <stella@unity3d.com>
2019-09-19 20:43:20 -0700

    fix help text

--- Editor/Scripts/Inspectors/OscRouterEditor.cs
+++ Editor/Scripts/Inspectors/OscRouterEditor.cs
@@ -6,8 +6,7 @@ namespace Resolink
     [CustomEditor(typeof(OscRouter))]
     public class OscRouterEditor : Editor
     {
-        const string k_HelpText = "This component handles routing OSC messages to your events, based on address, " +
-                                  "as well queueing them on the main thread.";
+        const string k_HelpText = "This component handles routing OSC messages to your events, based on address.";
 
         readonly GUIContent m_HandlerCountLabelContent = new GUIContent("Registered Handler Count", 
             "The number of addresses that event handlers have been registered for");
