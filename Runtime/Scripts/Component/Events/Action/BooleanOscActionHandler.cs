2000698687770eb0a69cf09d46427cb4e2457e8a
Stella Cannefax <stella@unity3d.com>
2019-11-06 00:23:30 -0800

    remove un-needed todo

--- Runtime/Scripts/Component/Events/Action/BooleanOscActionHandler.cs
+++ Runtime/Scripts/Component/Events/Action/BooleanOscActionHandler.cs
@@ -4,7 +4,6 @@ using OscCore;
 namespace Resolink
 {
     [Serializable]
-    // TODO - differentiate between int-as-bool and bool tag handler ?
     public class BooleanOscActionHandler : OscActionHandler<bool>
     {
         protected override bool GetMessageValue(OscMessageValues dataHandle)
